a34309727dbb50dbbac10ff8fc5087bdd716cffa
hunterinious <vlnerodenko@gmail.com>
2020-09-19 14:38:41 +0300

    pagination added

--- psychologists/views.py
+++ psychologists/views.py
@@ -1,9 +1,17 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from .models import PsychologistUserProfile
 from .serializers import PsyProfileForListSerializer
+
+
+class PsyProfileForListPagination(PageNumberPagination):
+    page_size = 20
+    page_query_description = 'size'
+    max_page_size = 28
 
 
 class PsyProfileListView(ListAPIView):
     queryset = PsychologistUserProfile.objects.all()
     serializer_class = PsyProfileForListSerializer
+    pagination_class = PsyProfileForListPagination
     permission_classes = []
