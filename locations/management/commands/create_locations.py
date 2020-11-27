3e6308592e748de871ae4e374f04311597090615
hunterinious <vlnerodenko@gmail.com>
2020-11-27 17:51:10 +0200

    cities with timezones in json format

--- locations/management/commands/create_locations.py
+++ locations/management/commands/create_locations.py
@@ -1,27 +1,14 @@
 from django.core.management.base import BaseCommand
 from locations.models import Country
+import json
 
 
 class Command(BaseCommand):
     help = 'Create locations'
 
     def handle(self, *args, **kwargs):
-        with open('static/files/cities_with_timezones.txt', 'r', encoding='utf-8') as file:
-            for f in file:
-                row = f.split('\t')
-                for i, r in enumerate(row):
-                    r.strip()
-                    row[i] = r.replace('\n', '')
-
-                utc = row[0]
-                location = row[1].split('/')
-                main_city = location[len(location) - 1]
-                country = row[2]
-
-                if len(row) == 4:
-                    cities = row[3]
-                else:
-                    cities = ''
-
-                Country.objects.create_country_with_city_from_file(name=main_city, country_name=country, utc=utc)
-
+        with open('static/files/cities_with_timezones.json') as f:
+            for obj in json.load(f):
+                Country.objects.create_country_with_city_from_file(name=obj['main_city'],
+                                                                   country_name=obj['country'],
+                                                                   utc=obj['utc'])
